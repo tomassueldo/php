4f36d5e2e27bb961c65d0c5fc4652bc20f1e669b
tomassueldo <tomassueldo17@gmail.com>
2021-11-25 17:52:11 -0300

    cambios

---
+++ mi-proyecto/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Formulario from './Formulario.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Formulario />
+      </header>
+    </div>
+  );
+}
+
+export default App;
